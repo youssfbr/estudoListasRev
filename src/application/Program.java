4399e845cc712d1460891a273f3f017eb6a59879
Alisson Youssf <youssfbr@gmail.com>
2018-09-25 11:35:16 -0300

    Removendo elemento da lista que atendam a um predicado

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,16 +13,27 @@ public class Program {
 		list.add("Alex");
 		list.add("Bob");
 		list.add("Ana");
-		list.add(2, "Marco");
-				
-		// Removendo elemento da lista pela posição
-		list.remove(1);
+		list.add(2, "Marco");		
 		
 		System.out.println(list.size());		
 		
 		for (String x : list) {
 			System.out.println(x);
 		}
+		
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("Remover todos que começam com a letra M.");
+		
+		// Removendo elemento da lista que atendam a um predicado
+		// Remover todo mundo que começa com a letra M.
+		
+		list.removeIf(x -> x.charAt(0) == 'M'); // Função lambda - Predicado - Retorna em V ou F
+		
+		System.out.println(list.size());		
+		
+		for (String x : list) {
+			System.out.println(x);
+		}	
 			
 	}
 }
